fdf97c76772f7c34ac1008af1124a055a85258c3
Chibschibs-tech <Chihab.jabri@gmail.Com>
2025-10-26 23:39:24 +0000

    chore: local adjustments

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./packages/db/src/schema.ts",
+  out: "./packages/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  casing: "snake_case",
+});
